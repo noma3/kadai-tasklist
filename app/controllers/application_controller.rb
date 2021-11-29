07ea9e6df7c80db59bae1951512350662061f846
noma <noma.developper@gmail.com>
2021-11-29 10:57:09 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  include SessionsHelper  #追記
   include Pagy::Backend  #追記
+  
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
