062430a6629518a87f17ca9d5904739a37f21869
noma <noma.developper@gmail.com>
2021-12-02 07:20:34 +0000

    code reduction

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
